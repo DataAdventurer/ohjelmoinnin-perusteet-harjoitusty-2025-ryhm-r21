500f8d2566355a2b2c1fa5d55a10d46568d373d3
Petter Paananen <petter.paananen@edu.turkuamk.fi>
2025-04-17 20:15:32 +0300

    Lisäsin valikko ominaisuuden main tiedostoon

--- main.py
+++ main.py
@@ -28,3 +28,11 @@ if __name__ == "__main__":
 
 
 print("This is a test message.")
+# valikko
+from tkinter import Tk
+from dropdownmenu import ComboBoxApp
+
+if __name__ == "__main__":
+    root = Tk()
+    app = ComboBoxApp(root)
+    root.mainloop()
