50f05d91b7e2e896540f7ccd5b9b2723be184928
Petter Paananen <petter.paananen@edu.turkuamk.fi>
2025-04-17 19:49:40 +0300

    Tuote olio, tuoteluettelo

---
+++ tuotteet.py
@@ -0,0 +1,22 @@
+class tuote:
+    def __init__(self, name, price, category):
+        self.name = nimi
+        self.price = hinta
+        self.category = tuoteryhmä
+
+    def __str__(self):
+        return f"{self.name} ({self.price:.2f} €)"
+    
+class tuoteluettelo:
+    def __init__(self):
+        self.tuotteet = []
+
+    def lisaa_tuote(self, tuote):
+        self.tuotteet.append(tuote)
+
+    def nayta_tuotteet(self):
+        for tuote in self.tuotteet:
+            print(tuote)
+    
+    def hae_tuotteet(self):
+        return [str(tuote) for tuote in self.tuotteet]
