394a119412220360777d76b876ebb4162d9cad77
DataAdventurer <kristofermaripuu@gmail.com>
2025-04-17 20:23:36 +0300

    Pankkitunnukset

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import random
 class Account:
     def __init__(self, name, balance):
         self.name = name
@@ -14,6 +15,31 @@ class Account:
 
 # Pääohjelma
 def main():
+    print("Luo pankkitunnukset")
+    käyttäjätunnus1 = input("Anna käyttäjän nimi:")
+    PINkoodi1 = input("Anna Uusi PIN-koodi:")
+    luottokortti_numero = random.randint(10**15, 10**16 - 1)
+    print(f'Luotu luottokortti numero: FI{luottokortti_numero}')
+    
+    print("Kirjaudu uudelleen pankkitilille nähdääksesi tilin tilanteen.")
+
+    yrityksiä = 0
+
+    while yrityksiä < 3:
+        käyttäjätunnus2 = input("Anna käyttäjätunnus: ")
+        PINkoodi2 = input("Anna PIN-koodi: ")
+
+        if käyttäjätunnus1 == käyttäjätunnus2 and PINkoodi1 == PINkoodi2:
+            print("Oikein!")
+            break
+        else:
+            yrityksiä = yrityksiä + 1
+            print("Yritä uudelleen.")
+
+            if yrityksiä == 3:
+                print("Muisti hämärtyi!")
+
+
     heikkis_account = Account("Heikki's account", 1000.0)
     personal_account = Account("Personal account", 0.0)
 
@@ -25,6 +51,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-
-print("This is a test message.")
