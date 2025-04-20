808f109cf7cac7fdf6beebd4c1f1ecd401844bd4
DataAdventurer <kristofermaripuu@gmail.com>
2025-04-20 20:50:17 +0300

    Pankkisivu ja pankkitili lisäys

--- main.py
+++ main.py
@@ -13,16 +13,14 @@ class Account:
     def __str__(self):
         return f"{self.name}: {self.balance:.2f} €"
 
-# Pääohjelma
 def main():
     print("Luo pankkitunnukset")
-    käyttäjätunnus1 = input("Anna käyttäjän nimi:")
-    PINkoodi1 = input("Anna Uusi PIN-koodi:")
+    käyttäjätunnus1 = input("Anna käyttäjän nimi: ")
+    PINkoodi1 = input("Anna Uusi PIN-koodi: ")
     luottokortti_numero = random.randint(10**15, 10**16 - 1)
-    print(f'Luotu luottokortt   i numero: FI{luottokortti_numero}')
-    
-    print("Kirjaudu uudelleen pankkitilille nähdääksesi tilin tilanteen.")
+    print(f'Luotu luottokortti numero: FI{luottokortti_numero}')
 
+    print("Kirjaudu uudelleen pankkitilille nähdääksesi tilin tilanteen.")
     yrityksiä = 0
 
     while yrityksiä < 3:
@@ -30,16 +28,15 @@ def main():
         PINkoodi2 = input("Anna PIN-koodi: ")
 
         if käyttäjätunnus1 == käyttäjätunnus2 and PINkoodi1 == PINkoodi2:
-            print("Oikein!")
+            print("Oikein! Kirjautuminen onnistui.")
             oma_tili = Account("Personal account", 10000.0)
-            break
+
         else:
-            yrityksiä = yrityksiä + 1
+            yrityksiä += 1
             print("Yritä uudelleen.")
 
             if yrityksiä == 3:
                 print("Muisti hämärtyi!")
-
 
     heikkis_account = Account("Heikki's account", 1000.0)
     personal_account = Account("Personal account", 0.0)
@@ -63,3 +60,8 @@ if __name__ == "__main__":
     root = Tk()
     app = ComboBoxApp(root)
     root.mainloop()
+
+
+from pankkitili import main
+
+main()
---
+++ pankkitili.py
@@ -0,0 +1,14 @@
+# pankkitili.py
+class Account:
+    def __init__(self, name, balance):
+        self.name = name
+        self.balance = balance
+
+    def withdrawal(self, amount):
+        self.balance -= amount
+
+    def deposit(self, amount):
+        self.balance += amount
+
+    def __str__(self):
+        return f"{self.name}: {self.balance:.2f} €"
